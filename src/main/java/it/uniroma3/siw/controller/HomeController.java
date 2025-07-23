22b782860cc24952c38764256a49938c9011ec0d
ElisaCap <elisacaprio0@gmail.com>
2025-07-23 15:01:01 +0200

    23/07

--- src/main/java/it/uniroma3/siw/controller/HomeController.java
+++ src/main/java/it/uniroma3/siw/controller/HomeController.java
@@ -41,6 +41,7 @@ public class HomeController {
 	//@PreAuthorize("isAuthenticated()")
 	@GetMapping("/home")
 	public String home(Model model) {
+		/*
 		 if(this.generati==false) {
 			 this.contradaService.inizializza();
 		this.fantinoService.inizializza();
@@ -63,7 +64,7 @@ this.generati=true;
 	   
 	  
 	    
-	}
+	}*/
 		 return "homepage.html";	 
 	
 	
